7a43a762aba26be8abffd13babd743c8178e5b6a
raydharma <puturaydharma235@gmail.com>
2022-01-12 00:28:10 +0800

    update composer.lock file

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '2ae5746986eb044000bd4c2ee16f9afd44c28708',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '2ae5746986eb044000bd4c2ee16f9afd44c28708',
+            'dev_requirement' => false,
+        ),
+    ),
+);
